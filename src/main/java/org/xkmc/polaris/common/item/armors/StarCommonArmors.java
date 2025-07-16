6959ed861e38b751f01e49b6c59ac020e7798d4e
cnlimiter <cnlimiter@163.com>
2025-07-16 20:38:54 +0800

    feat(armors): add tooltips for armor sets and creative flight
    - Add tooltip messages for wearing complete armor sets- Include information about creative flight enablement
    - Update tooltips for StarLord, Supreme Genesis, MS Armor, and Star Common Armors
    - Add corresponding translations in en_us.json and zh_cn.json

--- src/main/java/org/xkmc/polaris/common/item/armors/StarCommonArmors.java
+++ src/main/java/org/xkmc/polaris/common/item/armors/StarCommonArmors.java
@@ -1,10 +1,30 @@
 package org.xkmc.polaris.common.item.armors;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.xkmc.polaris.common.item.UnDamageArmors;
+import org.xkmc.polaris.handler.EffectArmorHandler;
+
+import java.util.List;
 
 public class StarCommonArmors extends UnDamageArmors {
     public StarCommonArmors(ArmorMaterial materialIn, Type slots, Properties settings) {
         super(materialIn, slots, settings);
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel, @NotNull List<Component> pTooltipComponents, @NotNull TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.polaris.wearing_all").withStyle(ChatFormatting.GRAY));
+        for (MobEffect effect: EffectArmorHandler.starLordEffects()) {
+            pTooltipComponents.add(Component.translatable(effect.getDescriptionId()).withStyle(ChatFormatting.BLUE));
+        }
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 }
